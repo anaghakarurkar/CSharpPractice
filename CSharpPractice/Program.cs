52f59365a11b8a5ecca14af73505d6f6d6bf59ee
anaghakarurkar <anaghakarurkar@gmail.com>
2022-09-12 21:16:30 +0100

    Initial setup done. Practiced if-else decision control structure

---
+++ CSharpPractice/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CSharpPractice;
+
+Person person = new ("Robert", "Walpole");
+
+Console.WriteLine("Who was UK's first prime minister?");
+
+string fullName = Console.ReadLine();
+
+if(person.FullName == fullName)
+{
+    Console.WriteLine("Hey! You entered correct name! You won!");
+  
+}
+else
+{
+    Console.WriteLine("Guessed wrong name... Try again!");
+}
+
+
+
