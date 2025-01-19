23afd02cddc36e24d5dc25cdc1c538cf0a0e939b
anaghakarurkar <anaghakarurkar@gmail.com>
2025-01-19 15:51:16 +0000

    Added problem solving solutions. changed dot net version to 8

--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -1,22 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using CSharpPractice;
 
-using CSharpPractice;
+Solution.PlusMinus([-4, 3, -9, 0, 4, 1]);
 
-Person person = new ("Robert", "Walpole");
+var diagonalDiff = Solution.DiagonalDifference([new List<int> { 11, 2, 4 }, new List<int> { 4, 5, 6 }, new List<int> { 10, 8, -12 }]);
+Console.WriteLine($"Diagonal Difference of a square matrix is :{diagonalDiff}");
+
+Solution.SingleNumber([4, 1, 2, 1, 2]);
+
+var twoSum = Solution.TwoSum([2, 7, 11, 15], 9);
+Console.WriteLine($"The indices of two numbers that add up to a target number : {twoSum[0]} {twoSum[1]}");
+
+Solution.PlusOne([1, 2, 3]);
+
+Person person = new("Robert", "Walpole");
 
 Console.WriteLine("Who was UK's first prime minister?");
 
 string fullName = Console.ReadLine();
 
-if(person.FullName == fullName)
+if (person.FullName == fullName)
 {
     Console.WriteLine("Hey! You entered correct name! You won!");
-  
 }
 else
 {
     Console.WriteLine("Guessed wrong name... Try again!");
 }
-
-
 
---
+++ CSharpPractice/Solution.cs
@@ -0,0 +1,68 @@
+﻿using System.Numerics;
+
+namespace CSharpPractice
+{
+    internal class Solution
+    {
+        /*Find ratio of positive numbers, negative numbers, and number of zeors from a list*/
+        public static void PlusMinus(List<int> arr)
+        {
+            double totalNumbers = arr.Count;
+            var zeros = arr.Where(e => e == 0).Count() / totalNumbers;
+            var positives = arr.Where(e => e > 0).Count() / totalNumbers;
+            var negatives = arr.Where(e => e < 0).Count() / totalNumbers;
+
+            Console.WriteLine($"Number of zeros ratio: {zeros:F6}\nPositive numbers ratio{positives:F6}\nNegative numbers ratio{negatives:F6}");
+        }
+
+        /*Find the absolute difference between a square matrix diagonals*/
+        public static int DiagonalDifference(List<List<int>> arr)
+        {
+            var leftToRight = 0;
+            var rightToLeft = 0;
+
+            for (int i = 0, j = arr.Count - 1; i < arr.Count; i++, j--)
+            {
+                leftToRight += arr[i][i];
+                rightToLeft += arr[j][i];
+            }
+            return Math.Abs(leftToRight - rightToLeft);
+        }
+
+        /*Find the number that appears only once in an array*/
+        public static void SingleNumber(int[] nums)
+        {
+            Console.Write("Single Number occured in an array: ");
+            Console.WriteLine(nums.GroupBy(g => g)
+                                  .Where(g => g.Count() == 1)
+                                  .Select(n => n.Key)
+                                  .First());
+        }
+
+        /*Find the indices of two numbers that add up to a target number*/
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var result = Array.Empty<int>();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[j] + nums[i] == target)
+                    {
+                        result = [i, j];
+                    }
+                    continue;
+                }
+            }
+            return result;
+        }
+
+        /*Add one to the number made with combining all the digits of an array*/
+
+        public static void PlusOne(int[] digits)
+        {
+            var x = BigInteger.Parse(string.Join("", digits)) + 1;
+            Console.WriteLine($"Number by combining digits + 1 : {x} ");
+        }
+    }
+}
